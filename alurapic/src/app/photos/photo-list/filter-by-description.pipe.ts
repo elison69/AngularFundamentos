25b6e42fb0cdd42d6121241a268d1017481fc5a6
elison.lamim <elison.lamim@senior.com.br>
2018-10-02 14:38:57 -0300

    create repository

---
+++ alurapic/src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo';
+
+@Pipe({name: 'filterByDescription' })
+export class FilterByDescriptionPipe implements PipeTransform{
+
+    transform(photos: Photo[], descriptionQuery: string) {
+        descriptionQuery = descriptionQuery.trim().toLowerCase();
+
+        if (descriptionQuery) {
+            return photos.filter(photos => photos.description.toLowerCase().includes(descriptionQuery));
+        }else {
+            return photos;
+        }
+    }
+
+}
